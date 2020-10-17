078614992b1441831067915ac478a50421916bae
nickhir <nick.hirschmueller@yahoo.de>
2020-10-17 23:07:47 +0200

    first commit

---
+++ phrasebase.py
@@ -0,0 +1,52 @@
+import os
+import json
+import re
+from tqdm import tqdm
+from colorama import Fore, init
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Searches through thousands of papers and tries to match a certain input string")
+
+    parser.add_argument('--IGNORECASE', dest='IGNORECASE', action='store_true',
+                        help="By default search is case sensetive. If you dont want this, set this flag.")
+
+    parser.add_argument("--exact", dest='exact', action='store_true',
+                        help="By default it also matches substrings. If you only want exct matches, set this flag")
+
+    args = parser.parse_args()
+
+    # important for colorama on windows
+    init()
+
+    # set path to the directory where the paper jsons are located.
+    path = r"D:\pmc_json"
+
+    pattern = input("What string are you searching for? (Regex are allowed): ")
+
+    if args.IGNORECASE:
+        pattern = re.compile(pattern, flags=re.IGNORECASE)
+
+    if args.exact:
+        pattern = pattern.strip()
+        pattern = re.compile(f" {pattern} ")
+
+    for file in tqdm(os.listdir(path)):
+        j = json.load(open(os.path.join(path, file)))
+
+        paper_id = j["paper_id"]
+
+        for text in j["body_text"]:
+            if re.search(pattern, text["text"]):
+                paragraph = text["text"]
+                print(f"\n\nPaper ID: {paper_id}")
+                match = re.search(pattern, paragraph).group()
+                print(re.sub(match, Fore.RED + match + Fore.RESET, paragraph), end="\n\n")
+
+    # TODO: paralize
+
+
+if __name__ == '__main__':
+    main()
